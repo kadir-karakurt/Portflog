0bebe06e6f5840e513bc49c0f9117eaf086d9ebd
misalkadir <misalkadir@gmail.com>
2024-02-29 18:17:38 +0300

    setup

--- portflog/portfolio/views.py
+++ portflog/portfolio/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('<h2>Home</h2>')
+
+def posts(request):
+    return HttpResponse('<h2>Posts</h2>')
+
+def post(request):
+    return HttpResponse('<h2>Post Title</h2>')
+
+def profile(request):
+    return HttpResponse('<h2>User Profile</h2>')
