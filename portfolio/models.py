945bad0e64b29500751c8068db278eaa7096e390
misalkadir <misalkadir@gmail.com>
2024-03-18 21:47:25 +0300

    changes

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="blogs")
+
+    def __str__(self):
+        return self.title
