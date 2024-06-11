0b098cc9c494f3abf1cf0d26c2a3346d1d2c598a
Kadir Karakurt <misalkadir@gmail.com>
2024-06-11 15:41:15 +0300

    project deploy

--- portflog/wsgi.py
+++ portflog/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portflog.settings')
 
 application = get_wsgi_application()
+
+app = application
